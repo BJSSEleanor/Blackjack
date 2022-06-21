d73c6c5285c1350d2dc88389a42a810873788b3a
eleanor.bracegirdle@bjss.com <eleanor.bracegirdle@bjss.com>
2022-06-21 10:24:48 +0100

    project initialised, card deck created

---
+++ blackjack/blackjack.py
@@ -0,0 +1,2 @@
+from blackjack import Deck
+new_deck = Deck()
---
+++ blackjack/deck.py
@@ -0,0 +1,8 @@
+from blackjack.card import Card, suits, ranks
+
+class Deck:
+    def __init__(self):
+        self.all_cards = []
+        for suit in suits:
+            for rank in ranks:
+                self.all_cards.append(Card(suit,rank))
